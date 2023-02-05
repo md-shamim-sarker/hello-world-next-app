ba7c46662ae29bdadf5aef1e1913f39c4759afff
shamimspro <shamimspro@gmail.com>
2023-02-05 22:58:30 +0600

    link component navigation

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Home = () => {
     return (
         <div>
+            <Link href={"/about"}>About</Link>
+            <Link href={"/contact"}>Contact</Link>
             <h2>This is Home page.</h2>
         </div>
     );
